3dbd02830616d03e865fe36eab27e1c57a886de7
Behrad Moniri <bemoniri@live.com>
2019-09-16 19:50:55 +0430

    SVM

--- main.m
+++ main.m
@@ -12,3 +12,17 @@ for subj = 1 : Num_Subj
     load(['Filtered_Normal/subj',num2str(subj),'.mat'])
     Healthy_Feature(subj, :) = Feature_Extractor(New_Sig');
 end
+
+%%
+clear all
+close all
+clc
+
+%%
+Feat = [Disorder_Feature; Healthy_Feature];
+Label = [ones(1, 196), zeros(1, 132)]';
+Jvals = J_Value(Feat, Label);
+Jvals(isnan(Jvals)) = 0;
+X = J_Chooser1(Feat,Jvals, 779)
+Y = [X, Label]
+
